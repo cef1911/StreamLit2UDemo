9e1278c58a4a87bbbf526949353f18fff4473eff
Christopher Enam Franklin <cef1911@hotmail.com>
2023-03-13 05:50:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,14 +73,23 @@ dc = {'Fish Caught':[1,2,3,4,5,6,7,8,9,10,11],
 fishdata = pd.DataFrame(data = dc)
 st.bar_chart(fishdata, y = 'Fish Caught', x='Fishing Casts')
 
-# Pie chart will be used kids team's data, This existing code below is where the slices will be ordered and plotted counter-clockwise:
-labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-sizes = [15, 30, 45, 1],
-explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+import matplotlib.pyplot as plt
+import numpy as np
 
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
 
-st.pyplot(fig1)
+st.pyplot(fig)
+
+# # Pie chart will be used kids team's data, This existing code below is where the slices will be ordered and plotted counter-clockwise:
+# labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+# sizes = [15, 30, 45, 1],
+# explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+# fig1, ax1 = plt.subplots()
+# ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+#         shadow=True, startangle=90)
+# ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+# st.pyplot(fig1)
