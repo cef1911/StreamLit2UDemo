b1ff9c66eb21695fcbbc3d963afdf07800c0ed47
Christopher Enam Franklin <cef1911@hotmail.com>
2023-03-12 22:07:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,10 +44,12 @@ st.dataframe(df2, use_container_width=True)
 
 st.bar_chart(df2)
 
-df3 = pd.DataFrame({'lab':['A', 'B', 'C'], 'val':[10, 30, 20]})
-ax = df3.plot.bar(x='lab', y='val', rot=0)
+st.bar_chart(df2, y = 'Team A', x='Fish Caught')
 
-st.bar_chart(df3)
+# df3 = pd.DataFrame({'lab':['A', 'B', 'C'], 'val':[10, 30, 20]})
+# ax = df3.plot.bar(x='lab', y='val', rot=0)
+
+# st.bar_chart(df3)
 
 # Team A	Role	Fish Caught
 # John	Captain	10
