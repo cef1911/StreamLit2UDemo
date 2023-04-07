59a272072150d9360003f78b2e651363e03f9ea2
Christopher Enam Franklin <cef1911@hotmail.com>
2023-04-07 06:19:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,3 +105,23 @@ coin = random.choice(coins)
 st.image(coin)
 if st.button('Flip'):
   st.experimental_rerun()
+
+
+import streamlit as st
+
+cam=st.selectbox(
+  'Choose a Cam', 
+  [
+    '',
+    'pitriverbridge/pitriverbridge.jpg',
+    'johnsongrade/johnsongrade.jpg',
+    'perez/perez.jpg',
+    'mthebron/mthebron.jpg',
+    'eurekaway/eurekaway.jpg',
+    'sr70us395/sr70us395.jpg',
+    'bogard/bogard.jpg',
+    'eastriverside/eastriverside.jpg',
+  ]
+)
+if cam:
+  st.image('https://cwwp2.dot.ca.gov/data/d2/cctv/image/' + cam)
