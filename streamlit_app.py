79f8daa84ef412f56af746a0655cb673b05982ce
Christopher Enam Franklin <cef1911@hotmail.com>
2023-04-12 11:26:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,49 +21,9 @@ More info to be added consistently
 
 
 
-# df = pd.DataFrame({
-#     'first column': [1, 2, 3, 4],
-#     'second column': [10, 20, 30, 40]
-#     })
-
-df2 = pd.DataFrame(
-    [
-        {"Team A": "John", "Role": "Captain", "Fish Caught": 21},
-        {"Team A": "Sara", "Role": "Data Wrangler", "Fish Caught": 4},
-        {"Team A": "Tony", "Role": "Team Liason", "Fish Caught": 10},
-        {"Team A": "Patricia", "Role": "Assistant Captain", "Fish Caught": 6},
-        {"Team A": "Tina", "Role": "Statistics Person", "Fish Caught": 30},
-    ]
-)
-
-# st.dataframe(df, use_container_width=True)
 st.dataframe(df2, use_container_width=True)
 
-# chart_data = pd.DataFrame(df
-# #     np.random.randn(20, 3),
-# #     columns=["a", "b", "c"])
-
-# st.bar_chart(df2)
-
 st.bar_chart(df2, y = 'Team A', x='Fish Caught')
-
-# # Team A Data
-# d = {'Team A':['John','Sara','Tony','Patricia','Tina'],
-#      'Fish Caught':[21, 4, 10, 6, 30],
-#      'Role':['Captain', 'Data Gatherer', 'Statistics Person', 'Team Liason', 'Assistant Captain']}
-
-# teamA = pd.DataFrame(data = d)
-
-# st.bar_chart(teamA, y = 'Team A', x='Fish Caught')
-
-# https://towardsdatascience.com/streamlit-from-scratch-presenting-data-d5b0c77f9622
-# # The Incredible Widget Company
-# d = {'Quarter':[1,2,3,4],
-#      'Widgets':[100,110,112,120],
-#      'Wodgets':[50,100,120, 125],
-#      'Wudgets':[200,150,100,90]}
-     
-# salesdf = pd.DataFrame(d)
 
 # Crypto monthly data
 dc = {'Fish Caught':[1,2,3,4,5,6,7,8,9,10,11],
@@ -72,27 +32,6 @@ dc = {'Fish Caught':[1,2,3,4,5,6,7,8,9,10,11],
 
 fishdata = pd.DataFrame(data = dc)
 st.bar_chart(fishdata, y = 'Fish Caught', x='Fishing Casts')
-
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# arr = np.random.normal(1, 1, size=100)
-# fig, ax = plt.subplots()
-# ax.hist(arr, bins=20)
-
-# st.pyplot(fig)
-
-# # Pie chart will be used kids team's data, This existing code below is where the slices will be ordered and plotted counter-clockwise:
-# labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-# sizes = [15, 30, 45, 1],
-# explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-
-# fig1, ax1 = plt.subplots()
-# ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-#         shadow=True, startangle=90)
-# ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-# st.pyplot(fig1)
 
 """
 # Demo of Chart Data DataFrame
